caf3cd1de26b9d581dcd5b5552923f2ab6191123
oasisofthestar <ken_africa@trendmicro.com>
2016-06-10 01:53:10 +0800

    DB Update

--- app/static/jquery.js
+++ app/static/jquery.js
@@ -1,17 +1,68 @@
 var ctr = 0;
-$(document).ready(function(m, n){
+$(document).ready(function(){
+	
+	function callAjax(urle, val){
+		$.ajax({
+            url: urle,
+            data: "var=" + val,
+            type: 'POST',
+            success: function(response) {
+            	response = response.replace("ObjectId", "")
+            				.replace("(", "")
+            				.replace(")", "")
+            				.replace(/'/g, '"');
+      		    
+
+      		    if(urle == '/selDB')
+      		    {
+      		    	var a = JSON.parse(response);
+      		    	for(i=0; i < a.length; i++)
+				    {
+				    	var val = a[i]['activity'];
+				    	$('#tbl1').after('<tr id="tblr' + i + '"><td><h3 id="lbl' + i + '">' + val + '</h3><input type="text" class="txt" id="txt' + i + '" style="display:none" value ="' + val + '"><input type="hidden" value="' + val + '" id="tdlvar' + i + '"></td><td><input type="button" class="btnedt" id="edt' + i + '" value="Edit"><input type="button" class="btndel" id="del' + i + '" value="Delete"></td></tr>');
+				    }
+      		    }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+	}
+
+	callAjax('/selDB', '0');
+	
     $('#add').click(function(){
-    	$('#tbl1').after('<tr><td><h3>' + $('#tdltext').val() + '</h3><input type="hidden" value="' + $('#tdltext').val() + '" id="tdlvar' + ctr + '"></td><td><input type="button" class="btnedt" id="' + ctr + '" value="Edit"><input type="button" class="btndel" id="' + ctr + '" value="Delete"></td></tr>');
+    	$('#tbl1').after('<tr id="tblr' + ctr + '"><td><h3 id="lbl' + ctr + '">' + $('#tdltext').val() + '</h3><input type="text" class="txt" id="txt' + ctr + '" style="display:none" value ="' + $('#tdltext').val() + '"><input type="hidden" value="' + $('#tdltext').val() + '" id="tdlvar' + ctr + '"></td><td><input type="button" class="btnedt" id="edt' + ctr + '" value="Edit"><input type="button" class="btndel" id="del' + ctr + '" value="Delete"></td></tr>');
     	ctr++;
+    	callAjax('/insDB', $('#tdltext').val());
     });
 
     $(document).on( 'click', '.btnedt', function() {
-    	var a = $(this).attr('id');
-    	alert($('#tdlvar' + a).val());
+	    var a = $(this).attr('id');
+	    a = a.substring(3, a.length);
+    	if($(this).val() != 'Done')
+    	{
+	    	var a = $(this).attr('id');
+	    	a = a.substring(3, a.length);
+	    	$('#lbl' + a).hide();
+	    	$('#txt' + a).show();
+	    	$('#edt' + a).val('Done');	
+    	}
+    	else
+    	{
+    		$('#tdlvar' + a).val($('#txt' + a).val());
+    		callAjax('/updDB', $('#tdlvar' + a).val());
+    		$('#lbl' + a).text($('#txt' + a).val());
+    		$('#lbl' + a).show();
+    		$('#txt' + a).hide();
+    		$('#edt' + a).val('Edit');
+    	}
 	});
 
 	$(document).on( 'click', '.btndel', function() {
       	var a = $(this).attr('id');
-    	alert($('#tdlvar' + a).val());
+	    a = a.substring(3, a.length);
+	    callAjax('delDB', $('#tdlvar' + a).val());
+	    $('#tblr' + a).remove();
 	});
 });
--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 from app import app
+from app import func
 
-@app.route('/')
+apicall = func.func()
+
+@app.route("/", methods=['POST', 'GET'])
+def indexd():
+	tdl = apicall.selDB()
+	return render_template("index.html", act=acts)
+
 @app.route('/index')
 def index():
 	return render_template('index.html')
@@ -9,3 +16,27 @@ def index():
 @app.route('/edit')
 def edit():
 	return render_template('edit.html')
+
+@app.route("/insDB", methods=["POST"])
+def callInsert():
+	tdl  = request.form['var']
+	return apicall.insDB(tdl)
+	#return redirect(url_for('edit'))
+
+@app.route("/selDB", methods=["POST"])
+def callSelect():
+	tdl  = request.form['var']
+	return apicall.selDB(tdl)
+
+@app.route("/delDB", methods=["POST"])
+def callDelete():
+	tdl  = request.form['var']
+	return apicall.delDB(tdl)
+
+@app.route("/updDB", methods=["POST"])
+def callUpdate():
+	tdl  = request.form['var']
+	return apicall.updDB(tdl)
+
+if __name__ == "__main__":
+    app.run()
